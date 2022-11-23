72ff5b0e17d7608b2856cfaa89d73d0201adc3d0
Sanjana Patil <78017261+Sanjana-Patil22@users.noreply.github.com>
2022-11-22 18:34:45 -0800

    Add files via upload

---
+++ grading/grading-script.sh
@@ -0,0 +1,21 @@
+args=("$@")
+INPUT=${args[0]}
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+
+while read sno reader student email cruzid timestamp commitid green_check ack
+do
+    if [ -z "$cruzid" ];
+    then
+        :
+    else
+        if [ "$cruzid" != "timestamp" ]; then
+            git clone git@git.ucsc.edu:cse13s/fall22/"$cruzid".git && echo "Execute git clone for cruzid $cruzid"
+            cd "$cruzid" && echo "change into $cruzid directory"
+            git checkout "$commitid" && echo "checkout commitid: $commitid"
+            cd ..
+        fi
+    fi
+done < $INPUT
+IFS=$OLDIFS
